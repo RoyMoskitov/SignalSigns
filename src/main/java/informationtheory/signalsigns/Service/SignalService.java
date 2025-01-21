5e027c4a953265459010977a912ee41dcc09eb63
Sergushko Vladimir <bobik1508@mail.ru>
2025-01-21 18:52:14 +0300

    Исправил ЧСП и добавил ФСП

--- src/main/java/informationtheory/signalsigns/Service/SignalService.java
+++ src/main/java/informationtheory/signalsigns/Service/SignalService.java
@@ -39,13 +39,19 @@ public class SignalService {
                 response.setEncodedText(request.getInputText());
                 response.setEncodedSignal(encodeTextFrequency(request));
                 response.setNoisySignal(makeSignalNoisy(response.getEncodedSignal(), request));
-                response.setDecodedText(decodeFrequencyToText(restoreFrequencySignal(response.getNoisySignal(), 25)));
+                response.setDecodedText(decodeFrequencyToText(restoreFrequencySignal(response.getNoisySignal(), 20)));
             }
             case AMPLITUDE -> {
                 response.setEncodedText(request.getInputText());
                 response.setEncodedSignal(encodeTextAmplitude(request));
                 response.setNoisySignal(makeSignalNoisy(response.getEncodedSignal(), request));
                 response.setDecodedText(decodeAmplitudeToText(response.getNoisySignal()));
+            }
+            case PHASE -> {
+                response.setEncodedText(request.getInputText());
+                response.setEncodedSignal(encodeTextPhase(request));
+                response.setNoisySignal(makeSignalNoisy(response.getEncodedSignal(), request));
+                response.setDecodedText(decodePhaseToText(response.getNoisySignal()));
             }
             default -> throw new RuntimeException("This type of signal is not supported yet");
         }
@@ -139,7 +145,7 @@ public class SignalService {
         return decodedText.toString();
     }
 
-    // Карта для отображения бита в соответствующую частоту
+
     private static final Map<Character, Double> frequencyMap = new HashMap<>();
     static {
         frequencyMap.put('0', 1.0);  // частота для бита 0
@@ -184,7 +190,7 @@ public class SignalService {
             }
 
             // Среднее значение
-            restoredSignal.add(sum / count);
+            restoredSignal.add(2*sum / count);
         }
 
         return restoredSignal;
@@ -200,10 +206,10 @@ public class SignalService {
             int f = 0;
             for (int j = i; j < i+100; j++) {
                 if(signal.get(j) > 0.4 & f == 0){
-                    sumMax += 1;
                     f = 1;
                 }
-                if(signal.get(j) < 0.4 & f == 1){
+                if(signal.get(j) < -0.4 & f == 1){
+                    sumMax += 1;
                     f = 0;
                 }
             }
@@ -231,7 +237,7 @@ public class SignalService {
     static {
         amplitudeMap.put('0', 0.0);  // амплитуда для бита 0
         amplitudeMap.put('1', 2.0);  // амплитуда для бита 1
-    }
+    }// Метод для кодирования строки АСП
     public static List<Double> encodeTextAmplitude(SignalRequest signalRequest) {
         String inputText = signalRequest.getInputText();
         if (inputText == null || inputText.isEmpty()) {
@@ -273,6 +279,69 @@ public class SignalService {
                 binaryString.append("1");
             } else {
                 binaryString.append("0");
+            }
+
+            // После накопления 8 битов восстанавливаем один символ
+            if (binaryString.length() == 8) {
+                String byteString = binaryString.toString();
+                char decodedChar = (char) Integer.parseInt(byteString, 2);
+                decodedText.append(decodedChar);
+                binaryString.setLength(0);  // Очищаем строку для следующего байта
+            }
+        }
+
+        return decodedText.toString();
+    }
+
+    private static final Map<Character, Double> phaseMap = new HashMap<>();
+    static {
+        phaseMap.put('0', 0.0);  // фаза для бита 0
+        phaseMap.put('1', 1.0);  // фаза для бита 1
+    }// Метод для кодирования строки ФСП
+    public static List<Double> encodeTextPhase(SignalRequest signalRequest) {
+        String inputText = signalRequest.getInputText();
+        if (inputText == null || inputText.isEmpty()) {
+            throw new IllegalArgumentException("Input text cannot be null or empty.");
+        }
+        List<Double> encodedMessage = new ArrayList<>();
+
+        for (char character : inputText.toCharArray()) {
+            String binaryString = String.format("%8s", Integer.toBinaryString(character)).replace(' ', '0');
+            for (char bit : binaryString.toCharArray()) {
+                Double phase = phaseMap.get(bit);
+                for (int i = 0; i < 100; i++) {  // В 1 секунду 10 шагов по 0.1 секунды
+                    double t = i * 0.01;  // Время для этого шага
+                    double w = 3 * Math.PI; // Угловая частота
+                    double amplitude = Math.sin(w * t + phase * Math.PI);
+                    encodedMessage.add(amplitude);
+                }
+            }
+        }
+        return encodedMessage.stream().mapToDouble(Double::doubleValue).boxed().toList();
+    }
+
+    public static String decodePhaseToText(List<Double> signal) {
+        if (signal == null || signal.isEmpty()) {
+            throw new IllegalArgumentException("Input signal cannot be null or empty.");
+        }
+        StringBuilder decodedText = new StringBuilder();
+        StringBuilder binaryString = new StringBuilder();
+
+        int samplesPerBit = 100;  // В каждом бите 100 отсчетов
+        for (int i = 0; i < signal.size(); i += samplesPerBit) {
+            // Рассчитываем период (интервал времени) для текущего бита
+            double sum = 0;
+            for (int j = i; j < i+33; j++) {
+                sum += signal.get(j);
+            }
+            for (int j = i+67; j < i+100; j++) {
+                sum += signal.get(j);
+            }
+            // Сравниваем с пороговыми значениями для определения 0 или 1
+            if (sum/samplesPerBit > 0.0) {
+                binaryString.append("0");
+            } else {
+                binaryString.append("1");
             }
 
             // После накопления 8 битов восстанавливаем один символ
